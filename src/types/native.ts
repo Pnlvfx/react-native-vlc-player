987c8aaee763b4fcedd7f4d2f4304adb345a8093
simone <simonegauli@gmail.com>
2025-04-30 16:07:27 +0200

    better docs

--- src/types/native.ts
+++ src/types/native.ts
@@ -12,6 +12,7 @@ export type NativePlayerProps = (VLCPlayerIosProps | VLCPlayerAndroidProps) & {
 export type VideoAspectRatio = '16:9' | '1:1' | '4:3' | '3:2' | '21:9' | '9:16';
 
 export interface NativePlayerCommands {
+  /** A value from 0 to 1 relative to the video duration. */
   seek: number;
   resume: boolean;
   snapshotPath: string;
