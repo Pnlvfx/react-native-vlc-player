0105b1f38e6ccf45359309872649f09959a17c1c
simone <simonegauli@gmail.com>
2025-07-20 20:13:39 +0200

    bump deps

--- tools/update.ts
+++ tools/update.ts
@@ -1,22 +1,17 @@
 /* eslint-disable no-console */
-import { updateLocalDeps } from '@goatjs/updater';
+import { updateGitDeps } from '@goatjs/dev/update';
 import { execAsync } from '@goatjs/node/exec';
 
 const packages = {
-  '@goatjs/ts-unused-exports': 'github:Pnlvfx/goatjs#workspace=@goatjs/ts-unused-exports',
-  '@goatjs/node': 'github:Pnlvfx/goatjs#workspace=@goatjs/node',
-  '@goatjs/rimraf': 'github:Pnlvfx/goatjs#workspace=@goatjs/rimraf',
   'eslint-plugin-react-compiler': 'experimental',
 };
 
 const run = async () => {
   try {
     await execAsync('yarn up @goatjs/updater@github:Pnlvfx/goatjs#workspace=@goatjs/updater');
-    await updateLocalDeps(packages);
-    process.exit(0);
+    await updateGitDeps(packages);
   } catch (err) {
     console.log(err);
-    process.exit(1);
   }
 };
 
