c33d916888252a1f765dd26faf55be7392e91bd4
simone <simonegauli@gmail.com>
2025-07-21 14:41:00 +0200

    bump deps

--- tools/update.ts
+++ tools/update.ts
@@ -1,18 +1,11 @@
-/* eslint-disable no-console */
 import { updateGitDeps } from '@goatjs/dev/update';
-import { execAsync } from '@goatjs/node/exec';
 
 const packages = {
   'eslint-plugin-react-compiler': 'experimental',
 };
 
 const run = async () => {
-  try {
-    await execAsync('yarn up @goatjs/updater@github:Pnlvfx/goatjs#workspace=@goatjs/updater');
-    await updateGitDeps(packages);
-  } catch (err) {
-    console.log(err);
-  }
+  await updateGitDeps(packages);
 };
 
 void run();
