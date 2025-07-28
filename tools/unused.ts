298413b69b1129a5f438f23e11a127a68d308e69
simone <simonegauli@gmail.com>
2025-07-28 17:36:51 +0200

    release on gcp

--- tools/unused.ts
+++ tools/unused.ts
@@ -1,9 +1,8 @@
 import { findUnusedExports } from '@goatjs/ts-unused-exports';
-import { inspectLog } from '@goatjs/node/log';
 
 const unused = await findUnusedExports({ ignoreFiles: ['with-vlc-media-player.ts', 'vlc-player.tsx'] });
 
 if (unused) {
-  inspectLog(unused);
+  console.log(unused);
   throw new Error('The following exports are unused, add them on the ignore or remove the exports to continue.');
 }
