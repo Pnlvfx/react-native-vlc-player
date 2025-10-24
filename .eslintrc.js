d6ca5213518411ebf1e1765b96a65724cf315158
Simone Gauli <simonegauli@gmail.com>
2025-10-24 09:54:11 +0200

    bump deps and dedupe

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   root: true,
   extends: ['@react-native', 'plugin:@typescript-eslint/strict-type-checked', 'plugin:@typescript-eslint/stylistic-type-checked'],
-  plugins: ['eslint-plugin-react-compiler'],
+  plugins: ['@typescript-eslint', 'eslint-plugin-react-compiler'],
   rules: {
     'react/no-array-index-key': 'error',
     'react/prefer-read-only-props': 'error',
@@ -11,13 +11,14 @@ module.exports = {
     '@typescript-eslint/prefer-readonly': 'error',
     'react-compiler/react-compiler': 'error',
     'react/jsx-no-literals': 'error',
-    'react/jsx-no-constructed-context-values': 'error',
+
     'no-console': 'warn',
 
     curly: 'off',
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     'no-void': 'off',
+    'no-empty': 'off',
   },
   parser: '@typescript-eslint/parser',
   parserOptions: { project: path.join(__dirname, 'tsconfig.json') },
