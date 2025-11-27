24ea93876550785b23aa05534c0a7c516a6b5f6a
Simone Gauli <simonegauli@gmail.com>
2025-11-27 16:52:31 +0100

    fix compiler configs

--- tools/unused.ts
+++ tools/unused.ts
@@ -1,7 +1,10 @@
 import { findUnusedExports } from '@goatjs/ts-unused-exports';
+import { prettier } from '@goatjs/node/prettier';
 
-const unused = await findUnusedExports({ ignoreFiles: ['with-vlc-media-player.ts', 'vlc-player.tsx'] });
+const unused = await findUnusedExports({ ignoreFiles: ['with-vlc-media-player.ts', 'vlc-player.tsx', 'vita.config.ts'] });
 
 if (unused) {
-  throw new Error('The following exports are unused, add them on the ignore or remove the exports to continue.');
+  throw new Error(
+    `The following exports are unused, add them on the ignore or remove the exports to continue.\n${await prettier.format(JSON.stringify(unused), { parser: 'json' })}`,
+  );
 }
---
+++ vita.config.ts
@@ -0,0 +1,3 @@
+import type { VitaConfig } from '@goatjs/vita';
+
+export default { reactCompiler: true } satisfies VitaConfig;
